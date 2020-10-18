085b950059410cc92e0b40e21c120c9f27d73343
Eduardo Bravo <66187093+EduardoBravoP@users.noreply.github.com>
2020-10-17 23:57:21 -0300

    backend

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+import routes from './routes'
+import './database/connection'
+import path from 'path'
+import errorHandler from './errors/handler'
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(routes)
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+
+app.use(errorHandler)
+
+app.listen(3333)
